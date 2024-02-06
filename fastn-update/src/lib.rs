c9627be89cb6e887654d8c45798bf62f8cf68eee
Harsh Singh <64768386+harshdoesdev@users.noreply.github.com>
2024-02-06 13:09:47 +0530

    Update lib.rs
    Signed-off-by: Harsh Singh <64768386+harshdoesdev@users.noreply.github.com>

--- fastn-update/src/lib.rs
+++ fastn-update/src/lib.rs
@@ -221,9 +221,6 @@ async fn download_and_unpack_zip(
                 Some((_, path)) => path,
                 None => path,
             };
-            if manifest.files.get(path_without_prefix).is_none() {
-                continue;
-            }
             let output_path = &dependency_path.join(path_without_prefix);
             ds.write_content(output_path, buffer)
                 .await
